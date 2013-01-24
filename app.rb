9480a3249d33d3f5e5759d605375d11181c72b1a
Wayne Graham <wayne.graham@virginia.edu>
2013-01-24 15:28:23 -0500

    add an initial menu structure and disallow robots from crawling data

--- app.rb
+++ app.rb
@@ -1,6 +1,37 @@
 require 'sinatra'
 require 'erb'
 
-get '/' do
-  erb 'index'
+configure :development do
+  enable :logging, :dump_errors, :raise_erros
 end
+
+configure :production do
+  set :raise_errors, false
+  set :show_exceptions, false
+end
+
+configure do
+  set :app_path, File.dirname(__FILE__)
+  set :course_files, settings.app_path + '/courses'
+  set :course_directories, '/courses'
+end
+
+before do
+  @menu = {}
+  Dir.glob "#{settings.course_files}/**" do |file|
+    me, mi = file.split('/')[-1]
+    @menu[me] ||= []
+    @menu[me] << mi
+  end
+end
+
+get '/' do
+  @classes = list_directories
+  erb :index
+end
+
+def list_directories
+  Dir.glob(settings.course_files + "/**").select{|f| File.directory? f}
+end
+
+
