88e59534766e91d6e25f2997b45bb03d8a145ed6
Wayne Graham <wayne.graham@virginia.edu>
2013-02-07 16:33:35 -0500

    add some more tests

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -26,4 +26,23 @@ describe 'The GIS Classes App' do
       last_response.should be_ok
     end
   end
+
+  describe "GET '/bhda'" do
+    it "should not be successfull" do
+      get '/bhda'
+      last_response.should_not be_ok
+    end
+  end
+
+  describe "GET '/cources/foo'" do
+    it "should be successfull" do
+      get '/courses/foo'
+      last_response.should be_ok
+    end
+
+    it "should have 'foo' as the heading" do
+      get '/courses/foo'
+      #last_response.body.should has_content? 'Foo'
+    end
+  end
 end
